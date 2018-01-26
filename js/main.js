6c2cf1c24c5aee344396377a30c278d159419610
Carrie Smith <carriesmith59@gmail.com>
2018-01-25 20:02:29 -0500

    logging things to the console in main.js

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// alerts are annoying, lets use consol.log
+console.log('Welcome');
+/*
+This will now log to your console
+*/
+
+// Create a function that updates a DOM element
+function change(){
+  // function body. where it all goes down
+  document.getElementById('foo').innerHTML = 'Welcome';
+}
+
+// Basic data types in javascript
+var name='Lucille'; // Store a string in a var
+var numberOfWidgets = 10; // Number
+var isCodingCool = true; // Boolean
+
+// store an array in a var
+var characters = [50, true, 'Lucille', 'Michael Bluth', 'Tobias', 10, 2];
+console.log(characters[0]);
+
+var animals = ['dog', 'cat', 'mouse'];
+var numberOfPets = 12;
+var areTheyFluffy = true;
+console.log(animals[0]);
